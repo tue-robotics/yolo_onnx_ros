aad04c9ac5e1fab55cf54623877153ac7b05a42f
iasonth95 <iasonth95@gmail.com>
2025-04-18 16:57:13 +0200

    cleaned repo so to add sam implementation

---
+++ CMakeFiles/CMakeTmp/CMakeFiles/cmTC_7182c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_7182c.dir/CMakeCXXCompilerABI.cpp.o"
+  "cmTC_7182c"
+  "cmTC_7182c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_7182c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
