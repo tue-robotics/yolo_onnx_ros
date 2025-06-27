9d28b15e231ba3129cbc4d113a527281c42b5bca
IasonTheodorou <iasonth95@gmail.com>
2025-06-27 14:49:51 +0200

    Commented out the classification parts

--- src/main.cpp
+++ src/main.cpp
@@ -58,41 +58,41 @@ void Detector(std::unique_ptr<YOLO_V8>& p) {
 }
 
 
-void Classifier(std::unique_ptr<YOLO_V8>& p)
-{
-    std::filesystem::path current_path = std::filesystem::current_path();
-    std::filesystem::path imgs_path = current_path;// / "images"
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<int> dis(0, 255);
-    for (auto& i : std::filesystem::directory_iterator(imgs_path))
-    {
-        if (i.path().extension() == ".jpg" || i.path().extension() == ".png")
-        {
-            std::string img_path = i.path().string();
-            //std::cout << img_path << std::endl;
-            cv::Mat img = cv::imread(img_path);
-            std::vector<DL_RESULT> res;
-            const char* ret = p->RunSession(img, res);
+// void Classifier(std::unique_ptr<YOLO_V8>& p)
+// {
+//     std::filesystem::path current_path = std::filesystem::current_path();
+//     std::filesystem::path imgs_path = current_path;// / "images"
+//     std::random_device rd;
+//     std::mt19937 gen(rd());
+//     std::uniform_int_distribution<int> dis(0, 255);
+//     for (auto& i : std::filesystem::directory_iterator(imgs_path))
+//     {
+//         if (i.path().extension() == ".jpg" || i.path().extension() == ".png")
+//         {
+//             std::string img_path = i.path().string();
+//             //std::cout << img_path << std::endl;
+//             cv::Mat img = cv::imread(img_path);
+//             std::vector<DL_RESULT> res;
+//             const char* ret = p->RunSession(img, res);
 
-            float positionY = 50;
-            for (int i = 0; i < res.size(); i++)
-            {
-                int r = dis(gen);
-                int g = dis(gen);
-                int b = dis(gen);
-                cv::putText(img, std::to_string(i) + ":", cv::Point(10, positionY), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(b, g, r), 2);
-                cv::putText(img, std::to_string(res.at(i).confidence), cv::Point(70, positionY), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(b, g, r), 2);
-                positionY += 50;
-            }
+//             float positionY = 50;
+//             for (int i = 0; i < res.size(); i++)
+//             {
+//                 int r = dis(gen);
+//                 int g = dis(gen);
+//                 int b = dis(gen);
+//                 cv::putText(img, std::to_string(i) + ":", cv::Point(10, positionY), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(b, g, r), 2);
+//                 cv::putText(img, std::to_string(res.at(i).confidence), cv::Point(70, positionY), cv::FONT_HERSHEY_SIMPLEX, 1, cv::Scalar(b, g, r), 2);
+//                 positionY += 50;
+//             }
 
-            cv::imshow("TEST_CLS", img);
-            cv::waitKey(0);
-            cv::destroyAllWindows();
-        }
+//             cv::imshow("TEST_CLS", img);
+//             cv::waitKey(0);
+//             cv::destroyAllWindows();
+//         }
 
-    }
-}
+//     }
+// }
 
 
 
@@ -147,7 +147,6 @@ int ReadCocoYaml(std::unique_ptr<YOLO_V8>& p) {
 
 void DetectTest()
 {
-    //YOLO_V8* yoloDetector = new YOLO_V8;
     std::unique_ptr<YOLO_V8> yoloDetector = std::make_unique<YOLO_V8>();
 
     ReadCocoYaml(yoloDetector);
@@ -176,15 +175,15 @@ void DetectTest()
 }
 
 
-void ClsTest()
-{
-    std::unique_ptr<YOLO_V8> yoloDetector = std::make_unique<YOLO_V8>();
-    std::string model_path = "cls.onnx";
-    ReadCocoYaml(yoloDetector);
-    DL_INIT_PARAM params{ model_path, YOLO_CLS, {224, 224} };
-    yoloDetector->CreateSession(params);
-    Classifier(yoloDetector);
-}
+// void ClsTest()
+// {
+//     std::unique_ptr<YOLO_V8> yoloDetector = std::make_unique<YOLO_V8>();
+//     std::string model_path = "cls.onnx";
+//     ReadCocoYaml(yoloDetector);
+//     DL_INIT_PARAM params{ model_path, YOLO_CLS, {224, 224} };
+//     yoloDetector->CreateSession(params);
+//     Classifier(yoloDetector);
+// }
 
 
 int main()
