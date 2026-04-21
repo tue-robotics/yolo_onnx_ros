583029b27fc70fa69b7849a9628816c3fc948420
20220890 <a.sirbu@student.tue.nl>
2026-04-21 21:11:34 +0200

    first commit - setup cmake build system

---
+++ build1/CMakeFiles/test_yolo_onnx_ros.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/COURSES/roboTeam/optimization/yolo_onnx_ros/src/main.cpp" "/mnt/c/COURSES/roboTeam/optimization/yolo_onnx_ros/build1/CMakeFiles/test_yolo_onnx_ros.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "include"
+  "/usr/local/onnxruntime/include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/mnt/c/COURSES/roboTeam/optimization/yolo_onnx_ros/build1/CMakeFiles/yolo_onnx_ros.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build1/CMakeFiles/yolo_onnx_ros.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libyolo_onnx_ros.a"
+)
